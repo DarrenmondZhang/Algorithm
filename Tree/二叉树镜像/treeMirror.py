6d5497ef05e599386648513be45cba8c015753f4
DarrenmondZhang <785320051@qq.com>
2020-02-20 22:56:37 +0800

    剑指offer tree题目提交

--- Tree/二叉树镜像/treeMirror.py
+++ Tree/二叉树镜像/treeMirror.py
@@ -18,23 +18,3 @@ class Solution:
         self.Mirror(root.left)
         # 处理右子树
         self.Mirror(root.right)
-
-
-if __name__ == '__main__':
-    t1 = TreeNode(8)
-    t2 = TreeNode(6)
-    t3 = TreeNode(10)
-    t4 = TreeNode(5)
-    t5 = TreeNode(7)
-    t6 = TreeNode(9)
-    t7 = TreeNode(11)
-
-    t1.left = t2
-    t1.right = t3
-    t2.left = t4
-    t2.right = t5
-    t3.left = t6
-    t3.right = t7
-
-    s = Solution()
-    print(s.Mirror(t1))
