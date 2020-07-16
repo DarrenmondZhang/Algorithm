53e661ac335900351c4ae9d2129aeabe2e945366
DarrenmondZhang <785320051@qq.com>
2020-07-16 12:01:40 +0800

    栈相关题目

--- stack/20_有效的括号/20_easy_有效的括号.py
+++ stack/20_有效的括号/20_easy_有效的括号.py
@@ -58,16 +58,30 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
+        dic = {')':'(', ']':'[', '}':'{'}
         stack = []
         for char in s:
-            if char == '(' :
-                stack.append(')')
-            elif char == '[' :
-                stack.append(']')
-            elif char == '{' :
-                stack.append('}')
-            elif stack == [] or stack.pop() != char:
+            if char in dic.values:
+                stack.append[]
+            elif char in dic.keys:
+                if stack == [] or dic[char] != stack.pop():
+                    return False
+            else:
                 return False
         return stack == []
+
+
+
+        # stack = []
+        # for char in s:
+        #     if char == '(' :
+        #         stack.append(')')
+        #     elif char == '[' :
+        #         stack.append(']')
+        #     elif char == '{' :
+        #         stack.append('}')
+        #     elif stack == [] or stack.pop() != char:
+        #         return False
+        # return stack == []
 # @lc code=end
 
