8f182eee33debdbaba42c68c0ff70ecd969b8d6e
DarrenmondZhang <785320051@qq.com>
2020-07-18 18:35:18 +0800

    hard 栈 双端队列

---
+++ sliding-window/239_滑动窗口最大值/239_hard_滑动窗口最大值.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode.cn id=239 lang=python3
+#
+# [239] 滑动窗口最大值
+#
+# https://leetcode-cn.com/problems/sliding-window-maximum/description/
+#
+# algorithms
+# Hard (46.15%)
+# Likes:    453
+# Dislikes: 0
+# Total Accepted:    61.2K
+# Total Submissions: 126.6K
+# Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+#
+# 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+# 个数字。滑动窗口每次只向右移动一位。
+# 
+# 返回滑动窗口中的最大值。
+# 
+# 
+# 
+# 进阶：
+# 
+# 你能在线性时间复杂度内解决此题吗？
+# 
+# 
+# 
+# 示例:
+# 
+# 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+# 输出: [3,3,5,5,6,7] 
+# 解释: 
+# 
+# ⁠ 滑动窗口的位置                最大值
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+# ⁠1 [3  -1  -3] 5  3  6  7       3
+# ⁠1  3 [-1  -3  5] 3  6  7       5
+# ⁠1  3  -1 [-3  5  3] 6  7       5
+# ⁠1  3  -1  -3 [5  3  6] 7       6
+# ⁠1  3  -1  -3  5 [3  6  7]      7
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= nums.length <= 10^5
+# -10^4 <= nums[i] <= 10^4
+# 1 <= k <= nums.length
+# 
+# 
+#
+
+# @lc code=start
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        ans = []
+        queue = deque()
+        for i in range(len(nums)):
+            while queue and queue[0] < i - k + 1:
+                queue.popleft()
+            while queue and nums[i] > nums[queue[-1]]:
+                print(queue[-1])
+                queue.pop()
+            queue.append(i)
+            if i >= k - 1:
+                ans.append(nums[queue[0]])
+        return ans
+
+
+Solution().maxSlidingWindow([1,3,2,0],2)
+
+
+# @lc code=end
+
